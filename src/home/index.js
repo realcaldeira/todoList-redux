f08bc522eb99b7603bb5341daba9525877dc5a20
realcaldeira <mrrealcaldeira@gmail.com>
2022-01-25 10:12:17 -0300

    List da home page

--- src/home/index.js
+++ src/home/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Divider from '@material-ui/core/Divider';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingBasket, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 import './styles.css';
 
@@ -17,6 +21,33 @@ const Home = (props) => (
             </div>
           </CardContent>
         </CardActionArea>
+      </Card>
+    </div>
+
+    <div className='list-container'>
+      <Card className='card'>
+        <CardActionArea className='card-content-area'>
+          <CardContent className='card-content'>
+            <div>
+              <p className='title'>Mês</p>
+              <div className='list-card-body'>
+                <div className='list-card-item'>
+                  <FontAwesomeIcon icon={faShoppingBasket} size='sm' />
+                  <p>1 Item(s) Restante</p>
+                </div>
+                <div className='list-card-item'>
+                  <FontAwesomeIcon icon={faCheck} size='sm' />
+                  <p>2 Item(s) Comprados</p>
+                </div>
+              </div>
+            </div>
+          </CardContent>
+        </CardActionArea>
+        <Divider />
+        <CardActions className="card-footer">
+          <p>01/01/2022</p>
+          <p>100.00</p>
+        </CardActions>
       </Card>
     </div>
 
